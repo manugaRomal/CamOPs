bd371b2e11855b2cedd6e8fb6ffb3af02c49a1cf
Anjana-Janekshaka <anjanajanekshaka@gmail.com>
2026-04-07 04:31:51 +0530

    feat: add StatCard component for displaying key dashboard metrics

--- frontend/src/assets/components/dashboard/StatCard.tsx
+++ frontend/src/assets/components/dashboard/StatCard.tsx
@@ -1,1 +1,19 @@
 //StatCard.tsx
+
+type StatCardProps = {
+  title: string;
+  value: number;
+  subtitle: string;
+};
+
+const StatCard = ({ title, value, subtitle }: StatCardProps) => {
+  return (
+    <div className="stat-card">
+      <h4>{title}</h4>
+      <h2>{value}</h2>
+      <p>{subtitle}</p>
+    </div>
+  );
+};
+
+export default StatCard;
